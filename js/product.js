8330fc69c6dd95a96cdb447aebd381392bffcb06
Matis Karpouzo <mkarpouzo@gmail.com>
2021-09-15 14:11:46 +0100

    test dispatch du produit séléctionné dans product.html

--- js/product.js
+++ js/product.js
@@ -1,13 +1,11 @@
 // Récupération de l'id du produit
 const chaineDeRequeteUrl_id = window.location.search;
-console.log('ok');
 
 // Extraire le texte de la chaine de requète pour ne récupérer que l'id
 const leId = chaineDeRequeteUrl_id.slice(4);
-console.log(leId);
 
 // affichage du produit sélectionné
-    // utilisation de la methode array.prototype.find()
+    // exemple d'utilisation de la methode find() trouvé dans mdn
     // const inventaire = [
     //     {nom: 'pommes', quantité: 2},
     //     {nom: 'bananes', quantité: 0},
@@ -15,6 +13,10 @@ console.log(leId);
     //   ];
     // const resultat = inventaire.find( fruit => fruit.nom === 'cerises');
     // console.log(resultat); 
-    
+
+let cameras = [];
+let response = fetch('./html/product.html?id=${cameras._id}') 
+console.log(response);
+
 const SelectedIdProduct = response.find((element) => element._id === leId);
 console.log(SelectedIdProduct);
